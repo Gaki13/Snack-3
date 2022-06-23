8507089118014f0dc86179b0d82caf2b9bf14891
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-06-23 12:28:15 +0200

    first commit

---
+++ snack-1/index.php
@@ -0,0 +1,67 @@
+<!-- Stampare ogni data con i relativi post. -->
+<?php
+
+    $posts = [
+
+        '10/01/2019' => [
+            [
+                'title' => 'Post 1',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 1'
+            ],
+            [
+                'title' => 'Post 2',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 2'
+            ],
+        ],
+        '10/02/2019' => [
+            [
+                'title' => 'Post 3',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 3'
+            ]
+        ],
+        '15/05/2019' => [
+            [
+                'title' => 'Post 4',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 4'
+            ],
+            [
+                'title' => 'Post 5',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 5'
+            ],
+            [
+                'title' => 'Post 6',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 6'
+            ]
+        ],
+    ];
+
+    foreach( $posts as  $post) {
+      var_dump($post);
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 1</title>
+</head>
+<body>
+    <h2>Post del 10/01/2019<h2>
+    <ul>
+        <?php  foreach( $posts as  $date[0]) {
+                foreach($date as $post){
+                   echo  $post['title'];
+                }
+        } ?>
+    </ul>
+</body>
+</html>
